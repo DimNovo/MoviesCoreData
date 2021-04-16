ebcb5099bb0c574feaa750980d063f470fda1c7c
Dmitry Novosyolov <i1710@icloud.com>
2021-04-17 00:29:51 +0300

    Initial Commit

---
+++ MoviesCoreData/MoviesCoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesCoreDataApp.swift
+//  MoviesCoreData
+//
+//  Created by Dmitry Novosyolov on 17/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct MoviesCoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
